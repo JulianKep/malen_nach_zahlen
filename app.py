798fcac8a707f4b88ee564141dca65c25ee6eca5
JulianKep <132519348+JulianKep@users.noreply.github.com>
2025-04-19 22:48:52 +0200

    added choose_colors

--- app.py
+++ app.py
@@ -2,19 +2,22 @@ import example
 import cv2
 import numpy as np
 
-image = [
+""" image = [
     [[255, 0, 0], [0, 255, 0]],
     [[0, 0, 255], [255, 255, 255]],
     [[128, 128, 128], [64, 64, 64]]
-]
+] """
 
 
-""" image = cv2.imread('image.jpg')
-cv2.imshow('My Image', image)
+image = cv2.imread('image.jpg')
+
+""" cv2.imshow('My Image', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows() """
 
 """ print(example.add(3, 5))
 print(example.sub(8, 7)) """
 
-print(example.process_image(image))
+""" print(example.process_image(image)) """
+
+print(example.choose_colors(image))
--- example.cpp
+++ example.cpp
@@ -2,9 +2,10 @@
 
 #include <pybind11/pybind11.h>
 
-//this enables automatic conversion between python lists and c++ STL containers (vectors)
+// this enables automatic conversion between python lists and c++ STL containers (vectors)
 #include <pybind11/stl.h>
 #include <vector>
+#include <cmath>
 
 int add(int i, int j)
 {
@@ -16,10 +17,18 @@ int sub(int i, int j)
     return i - j;
 }
 
+std::vector<std::vector<std::vector<uint8_t> > > choose_colors(std::vector<std::vector<std::vector<uint8_t> > > &image)
+{
+
+    image[0][0] = {200, 10, 100};
+
+    return image;
+}
+
 // std::vector is a dynamically sized array
 // cosnt -> i promise not to modify the data in the function
 //  "&" after vector<..> pass by reference, wount be copied into the functon, original data will be modified
-int process_image(const std::vector<std::vector<std::vector<uint8_t>>> &image)
+int process_image(const std::vector<std::vector<std::vector<uint8_t> > > &image)
 {
     size_t height = image.size();
     size_t width = image.size();
@@ -35,4 +44,5 @@ PYBIND11_MODULE(example, m)
     m.def("add", &add, "A function that adds two numbers");
     m.def("sub", &sub, "A function that subs two numbers");
     m.def("process_image", &process_image, "A function to process an image");
+    m.def("choose_colors", &choose_colors, "choose colors");
 }
