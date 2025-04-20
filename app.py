4d7454c977a41150fcfdc23d084441f285c8cbd9
JulianKep <132519348+JulianKep@users.noreply.github.com>
2025-04-20 22:42:43 +0200

    tried color conversion

--- app.py
+++ app.py
@@ -8,12 +8,14 @@ import numpy as np
     [[128, 128, 128], [64, 64, 64]]
 ] """
 
+colors = np.array([[100, 100, 100], [200, 200, 200]])
 
 image = cv2.imread('image.jpg')
+image = np.array(example.choose_colors(image, colors))
 
-""" cv2.imshow('My Image', image)
+cv2.imshow('My Image', image)
 cv2.waitKey(0)
-cv2.destroyAllWindows() """
+cv2.destroyAllWindows()
 
 """ print(example.add(3, 5))
 print(example.sub(8, 7)) """
--- example.cpp
+++ example.cpp
@@ -7,20 +7,32 @@
 #include <vector>
 #include <cmath>
 
-int add(int i, int j)
+float distance(const std::vector<uint8_t> &a, const std::vector<uint8_t> &b)
 {
-    return i + j;
+    return (sqrt(pow((a[0] - b[0]), 2) + pow((a[1] - b[1]), 2) + pow((a[2] - b[2]), 2)));
 }
 
-int sub(int i, int j)
+std::vector<std::vector<std::vector<uint8_t>>> choose_colors(std::vector<std::vector<std::vector<uint8_t>>> &image, std::vector<std::vector<uint8_t>> &color_vector)
 {
-    return i - j;
-}
-
-std::vector<std::vector<std::vector<uint8_t> > > choose_colors(std::vector<std::vector<std::vector<uint8_t> > > &image)
-{
-
-    image[0][0] = {200, 10, 100};
+    for (size_t width = 0; width < image.size(); width++)
+    {
+        for (size_t height = 0; height < image[width].size(); height++)
+        {
+            float current_min_distance = 9999;
+            for (size_t i = 0; i < color_vector.size(); i++)
+            {
+                float current_distance = distance(image[width][height], color_vector[i]);
+                size_t color_index = 0;
+                if (current_distance <= current_min_distance)
+                {
+                    current_min_distance = current_distance;
+                    color_index = i;
+                }
+                image[width][height] = color_vector[i];
+            }
+            
+        }
+    }
 
     return image;
 }
@@ -28,7 +40,7 @@ std::vector<std::vector<std::vector<uint8_t> > > choose_colors(std::vector<std::
 // std::vector is a dynamically sized array
 // cosnt -> i promise not to modify the data in the function
 //  "&" after vector<..> pass by reference, wount be copied into the functon, original data will be modified
-int process_image(const std::vector<std::vector<std::vector<uint8_t> > > &image)
+int process_image(const std::vector<std::vector<std::vector<uint8_t>>> &image)
 {
     size_t height = image.size();
     size_t width = image.size();
@@ -40,9 +52,6 @@ int process_image(const std::vector<std::vector<std::vector<uint8_t> > > &image)
 PYBIND11_MODULE(example, m)
 {
     m.doc() = "pybind11 example plugin"; // optional module docstring
-
-    m.def("add", &add, "A function that adds two numbers");
-    m.def("sub", &sub, "A function that subs two numbers");
     m.def("process_image", &process_image, "A function to process an image");
     m.def("choose_colors", &choose_colors, "choose colors");
 }
